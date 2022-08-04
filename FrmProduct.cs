a223f056216d2c0d399cf40a76149946b094490c
Esen Kurtulmus <109716591+esen-kurtulmus@users.noreply.github.com>
2022-08-04 17:07:08 +0300

    Add files via upload

---
+++ FrmProduct.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace demo_Adonet
+{
+    public partial class frmproduct: Form
+    {
+        public frmproduct()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=LAPTOP-IQULJ22J\\SQLEXPRESS;Initial Catalog=DbistanbulAkademi;Integrated Security=True");
+            connection.Open();
+            SqlCommand Command = new SqlCommand("select * from tblproduct", connection);
+            SqlDataAdapter adapter= new SqlDataAdapter(Command);
+            DataTable dataTable=new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;   
+
+            connection.Close();
+        }
+    }
+}
